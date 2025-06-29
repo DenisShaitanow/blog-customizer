e43575d70b7279d3a7fd1564ab4e228c2464f49e
DenisShaitanow <shaitanow@yandex.ru>
2025-06-29 11:15:22 +0300

    Add files via upload

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,103 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState } from 'react';
+import { Select} from '../../ui/select/Select';
+import {RadioGroup} from '../../ui/radio-group/RadioGroup';
+import {Separator} from '../../ui/separator/Separator';
+import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from 'src/constants/articleProps';
+import { OptionType } from 'src/constants/articleProps';
+import { defaultArticleState } from 'src/constants/articleProps';
+
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+
+interface Props {
+    onApply: (style: StateForm) => void;
+    onReset: () => void;
+}
+
+type StateForm = {
+	'--font-family': string,
+	'--font-size': string,
+	'--font-color': string,
+	'--container-width': string,
+	'--bg-color': string,
+}
+
+export const ArticleParamsForm = ({ onApply, onReset }: Props) => {
+	
+	const [isOpen, setIsOpen] = useState(false);
+	const [stateForm, setStateForm] = useState<StateForm>(
+		{
+			'--font-family': defaultArticleState.fontFamilyOption.value,
+			'--font-size': defaultArticleState.fontSizeOption.value,
+			'--font-color': defaultArticleState.fontColor.value,
+			'--container-width': defaultArticleState.contentWidth.value,
+			'--bg-color': defaultArticleState.backgroundColor.value,
+		}
+	);
+
+	const openSideBar = () => {
+		setIsOpen(!isOpen)
+	}
+
+	const [selectedFontSize, setSelectedFontSize] = useState<OptionType>(defaultArticleState.fontSizeOption);
+	const [selectedFontFamily, setSelectedFontFamily] = useState<OptionType>(defaultArticleState.fontFamilyOption);
+	const [selectedFontColor, setSelectedFontColor] = useState<OptionType>(defaultArticleState.fontColor);
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState<OptionType>(defaultArticleState.backgroundColor);
+	const [selectedContentWidth, setSelectedContentWidth] = useState<OptionType>(defaultArticleState.contentWidth);
+
+
+	// Обработчик изменений выбранного значения
+	const handleSelectionChangeFamily = (newSelectedValue: OptionType) => {
+		setSelectedFontFamily(newSelectedValue); 
+		setStateForm(prevState => ({ ...prevState, '--font-family': newSelectedValue.value }));
+	};
+
+	const handleSelectionChangeSize = (newSelectedValue: OptionType) => {
+		setSelectedFontSize(newSelectedValue); 
+		setStateForm(prevState => ({ ...prevState, '--font-size': newSelectedValue.value }));
+	};
+	
+	const handleSelectionChangeColor = (newSelectedValue: OptionType) => {
+		setSelectedFontColor(newSelectedValue); 
+		setStateForm(prevState => ({ ...prevState, '--font-color': newSelectedValue.value }));
+	};
+
+	const handleSelectionBackgroundColor = (newSelectedValue: OptionType) => {
+		setSelectedBackgroundColor(newSelectedValue); 
+		setStateForm(prevState => ({ ...prevState, '--bg-color': newSelectedValue.value }));
+	};
+
+	const handleSelectionContentWidth = (newSelectedValue: OptionType) => {
+		setSelectedContentWidth(newSelectedValue); 
+		setStateForm(prevState => ({ ...prevState, '--container-width': newSelectedValue.value }));
+	};
+
+	const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        onApply(stateForm); // Применяем новые стили
+    };
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<>	
+			
+			<ArrowButton isOpen={isOpen} onClick={openSideBar} />
+			<aside className={`${styles.container} ${isOpen ? styles.container_open : ''}`}>
+				
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Select selected={selectedFontFamily} onChange={handleSelectionChangeFamily} options={fontFamilyOptions} title='Шрифты' placeholder='Open Sans'/>
+					<RadioGroup options={fontSizeOptions} name="size" selected={selectedFontSize} title='Размер шрифта' onChange={handleSelectionChangeSize}/>
+					<Select selected={selectedFontColor} onChange={handleSelectionChangeColor} options={fontColors} title='Цвет шрифта' placeholder='shrifts'/>
+					<Separator />
+					<Select selected={selectedBackgroundColor} onChange={handleSelectionBackgroundColor} options={backgroundColors} title='Цвет фона'/>
+					<Select selected={selectedContentWidth} onChange={handleSelectionContentWidth} options={contentWidthArr} title='Ширина контента'/>
+				
+				
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+					<Button title='Сбросить' htmlType='button' onClick={() => onReset()} type='clear' />
+					<Button title='Применить' htmlType='submit' type='apply' onClick={() => onReset()}/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
+import { useState } from 'react';
+
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +14,52 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+type PageStyle = {
+	'--font-family': string,
+	'--font-size': string,
+	'--font-color': string,
+	'--container-width': string,
+	'--bg-color': string,
+}
+
 const App = () => {
+	const initialPageStyle: PageStyle = {
+        '--font-family': defaultArticleState.fontFamilyOption.value,
+        '--font-size': defaultArticleState.fontSizeOption.value,
+        '--font-color': defaultArticleState.fontColor.value,
+        '--container-width': defaultArticleState.contentWidth.value,
+        '--bg-color': defaultArticleState.backgroundColor.value,
+    };
+
+    const [pageStyle, setPageStyle] = useState(initialPageStyle);
+
+    // Функция сброса настроек до начальных значений
+    const resetStyles = () => {
+        setPageStyle(initialPageStyle);
+    };
+
+    // Функция обновления стилей страницы
+    const applyStyles = (newStyle: PageStyle) => {
+        setPageStyle(newStyle);
+    };
+	
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+				{ 	
+					'--font-family': pageStyle['--font-family'],
+        			'--font-size': pageStyle['--font-size'],
+        			'--font-color': pageStyle['--font-color'],
+        			'--container-width': pageStyle['--container-width'],
+        			'--bg-color': pageStyle['--bg-color'],
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm 
+                onApply={applyStyles} // Применяет новые стили
+                onReset={resetStyles} // Сбрасывает стили
+            />
+				
 			<Article />
 		</main>
 	);
